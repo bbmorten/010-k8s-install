19e4b1b711b404a4b87a1d4e26d74b7ba7e11d89
gitea@localhost <bbmorten@gmail.com>
2025-04-15 17:54:23 +0300

    Sync

--- docker-registry.sh
+++ docker-registry.sh
@@ -5,6 +5,9 @@ kubectl create secret docker-registry dockerhub-creds \
   --docker-email=bbmorten@gmail.com
 
 kubectl patch serviceaccount default -p '{"imagePullSecrets": [{"name": "dockerhub-creds"}]}'
+kubectl patch serviceaccount default -p '{"imagePullSecrets": []}'
+kubectl delete secret dockerhub-creds
+
 
 # Download nerdctl (check latest version at: https://github.com/containerd/nerdctl/releases)
 wget https://github.com/containerd/nerdctl/releases/download/v1.5.0/nerdctl-1.5.0-linux-amd64.tar.gz
@@ -21,6 +24,7 @@ sudo nerdctl --namespace=k8s.io pull bitnami/kubectl:latest
 
 sudo nerdctl run -d -p 5000:5000 --name registry registry:2
 
+sudo nerdctl pull bitnami/kubectl:latest
 sudo nerdctl tag bitnami/kubectl:latest 192.168.48.61:5000/kubectl:latest
-sudo nerdctl push 192.168.48.61:5000/kubectl:latest
+sudo nerdctl push 192.168.48.61:5000/kubectl:latest # failed
 sudo nerdctl push 192.168.48.61:5000/kubectl:latest --insecure-registry
